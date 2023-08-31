f2bb91c3f9f72b3f2af12d6fdeca64e11ee3f88c
Rysias <jhvithamar@gmail.com>
2023-08-31 16:38:53 +0200

    format

--- src/gpt2anki/fileio.py
+++ src/gpt2anki/fileio.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
+
 def read_txt(filepath: Path) -> str:
     """Read a text file."""
-    with open(filepath, "r") as f:
+    with open(filepath) as f:
         return f.read()
