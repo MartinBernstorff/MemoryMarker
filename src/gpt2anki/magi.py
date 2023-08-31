107ecf790f43a5612f178584ad92efada90ac661
Rysias <jhvithamar@gmail.com>
2023-08-31 17:27:14 +0200

    add tests

--- src/gpt2anki/magi.py
+++ src/gpt2anki/magi.py
@@ -1,14 +1,15 @@
 import ast
-import typing as T
 from pathlib import Path
 
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.schema.output import LLMResult
+from dotenv import load_dotenv
 
 import src.gpt2anki.fileio as fileio
 from src.gpt2anki.sources.hypothesis import Highlight
 
+load_dotenv()
 SYSTEM_PROMPT = fileio.read_txt(Path("../prompts/martin_prompt.txt"))
 
 
@@ -22,17 +23,19 @@ def highlight_to_prompt(highlight: Highlight) -> str:
         context=highlight.context,
     )
 
-def parse_output(output: LLMResult) -> T.Dict[str, str]:
+
+def parse_output(output: LLMResult) -> dict[str, str]:
     text_output = output.generations[0][0].text
     # extract dictionary from string
     start = text_output.find("{")
     end = text_output.rfind("}") + 1
     return ast.literal_eval(text_output[start:end])
 
+
 async def prompt_gpt(
     model: ChatOpenAI,
     highlight: Highlight,
-) -> T.Dict[str, str]:
+) -> dict[str, str]:
     prompt = highlight_to_prompt(highlight)
     messages = [SystemMessage(content=SYSTEM_PROMPT), HumanMessage(content=prompt)]
     output = await model.agenerate(messages=[messages])
---
+++ src/gpt2anki/test_magi.py
@@ -0,0 +1,18 @@
+import pytest
+
+import src.gpt2anki.magi as magi
+from src.gpt2anki.sources.hypothesis import Highlight
+
+# create a pytest fixture for the model
+@pytest.fixture(scope="session")
+def model():
+    return magi.initialize_model()
+
+async def test_model_response(model):
+    higlight = Highlight(context="Mitochondria is the powerhouse of the cell", highlight="Mitochondria")
+    output = await magi.prompt_gpt(model, higlight)
+    # check that outpuis a dictionary with keys "answer" and "question"
+    assert "answer" in output
+    assert "question" in output
+
+
