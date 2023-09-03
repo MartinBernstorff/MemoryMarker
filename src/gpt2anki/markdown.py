0721de05b929dbca3519750fdb42a85443a080b1
Rysias <jhvithamar@gmail.com>
2023-09-03 15:43:28 +0200

    save stuff

--- src/gpt2anki/markdown.py
+++ src/gpt2anki/markdown.py
@@ -3,16 +3,17 @@ from pathlib import Path
 
 SAVE_DIR = Path("card_cache")
 
+
 def hash_uri(uri: str) -> str:
     """return an md5 hash of the uri"""
     return hashlib.md5(uri.encode()).hexdigest()
-    
+
 
 def q_to_markdown(question: dict[str, str]) -> str:
     return f"Q: {question['question']}\nA: {question['answer']}\n\n"
 
 
-def write_md(markdown: str, origin_uri: str, save_dir: Path=SAVE_DIR) -> None:
+def write_md(markdown: str, origin_uri: str, save_dir: Path = SAVE_DIR) -> None:
     """Write markdown to file. Append if exists"""
     if not save_dir.exists():
         save_dir.mkdir()
--- src/gpt2anki/test_markdown.py
+++ src/gpt2anki/test_markdown.py
@@ -1,12 +1,15 @@
 import pytest
 
 import gpt2anki.markdown as markdown
+
+
 @pytest.fixture(scope="module")
 def question() -> dict[str, str]:
     return {
         "question": "What is the meaning of life?",
         "answer": "42",
     }
+
 
 def test_single_q_to_markdown(question) -> None:
     input_md = markdown.q_to_markdown(question)
