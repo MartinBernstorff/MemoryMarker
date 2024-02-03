cd15653476e7fc3bb337b4f7422571d9087f6802
Martin Bernstorff <martinbernstorff@gmail.com>
2024-02-03 14:24:59 +0000

    feat: add collapsible highlight with context to output

--- memorymarker/highlight_providers/base.py
+++ memorymarker/highlight_providers/base.py
@@ -16,10 +16,22 @@ class OrphanHighlight:
 class HydratedHighlight(BaseModel):
     source_doc_title: str
     source_doc_uri: str
+
+    prefix: str | None
     highlighted_text: str
-    context: str
+    suffix: str | None
+
     source_highlight_uri: str | None = None
     updated_at: dt.datetime
+
+    @property
+    def context(self) -> str:
+        context = ""
+        context += self.prefix or ""
+        context += self.highlighted_text
+        context += self.suffix or ""
+
+        return context
 
 
 class HighlightSource(Protocol):
