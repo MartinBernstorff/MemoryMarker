107fae7cdfd278caa92eee9112bfdc8acc974033
Martin Bernstorff <martinbernstorff@gmail.com>
2024-02-26 17:03:43 +0100

    misc.

---
+++ memorymarker/document_providers/readwise.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import Mapping
+
+from iterpy.iter import Iter
+from omnivoreql import OmnivoreQL
+
+from memorymarker.document_providers.Document import Document
+
+from .base import DocumentProvider
+
+
+@dataclass
+class Readwise(DocumentProvider):
+    # Use this library: https://github.com/rwxd/pyreadwise
+    api_key: str
+
+    def __post_init__(self):
+        self.client = OmnivoreQL(self.api_key)
+
+    def _parse_doc(self, document: Mapping[str, str]) -> Document:
+        return Document(
+            title=document["title"],
+            uri=document["url"],
+            highlights=document["highlights"],  # type: ignore
+            slug=document["slug"],
+        )
+
+    def get_documents(self) -> Iter[Document]:
+        documents = (
+            Iter(self.client.get_articles(limit=1000)["search"]["edges"])
+            .map(lambda a: a["node"])
+            .map(self._parse_doc)
+            .flatten()
+        )
+        return documents
