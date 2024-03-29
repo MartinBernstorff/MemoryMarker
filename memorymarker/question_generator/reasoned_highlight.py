ad9fee585c61c82c5741a2350baee0c44232428d
Martin Bernstorff <martinbernstorff@gmail.com>
2024-03-29 14:23:32 +0100

    update reasoned_highlight.py

--- memorymarker/question_generator/reasoned_highlight.py
+++ memorymarker/question_generator/reasoned_highlight.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Sequence
 
-import attrs
-from attrs import define
 from openai import BaseModel
 
 if TYPE_CHECKING:
@@ -36,13 +34,3 @@ class ReasonedHighlight(BaseModel):
     @property
     def context(self) -> str:
         return f"{self.prefix}{self.highlighted_text}{self.suffix}"
-
-
-@define
-class C:
-    test: str
-
-
-if __name__ == "__main__":
-    test_class = C(test="Testing")
-    two = attrs.evolve(test_class, test2="Testing2")
